2d608264736efd279bbe8b5899a9003266bfc93c
Matvey Akimenko <102146670+matveyakm@users.noreply.github.com>
2026-01-09 03:48:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ app.post('/api/dot', (req, res) => {
   }
 });
 
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
 // Главная страница с холстом для просмотра
 app.get('/', (req, res) => {
     res.send(`
